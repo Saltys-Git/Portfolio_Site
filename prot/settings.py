1299a5b4d5b6939526a4ed6a6f06c3dd86bce721
Salty <64358153+Saltys-Git@users.noreply.github.com>
2023-02-26 23:07:53 +0600

    Update settings.py

--- prot/settings.py
+++ prot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9d^=c@5f)p2=tp#wd50q2xi2fhf5yz_+@l-pfm@-=71utkiq+b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
